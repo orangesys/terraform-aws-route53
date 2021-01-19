0b1aaaf2e977055c6ee02217e27239ebd89fb89e
xucheng <jobxucheng@gmail.com>
2021-01-19 13:37:34 +0900

    add new

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ module "labels" {
   source = "git::https://github.com/clouddrove/terraform-labels.git?ref=tags/0.14.0"
 
   name        = var.name
-  application = var.application
+  # application = var.application
   environment = var.environment
   managedby   = var.managedby
   label_order = var.label_order
